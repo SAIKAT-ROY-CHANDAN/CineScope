05f8f299c9c1990ffad34abd1a80282c667169f8
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-07-08 14:23:26 +0600

    add a skeleten card in home

--- src/components/TopMovies/TopMovies.tsx
+++ src/components/TopMovies/TopMovies.tsx
@@ -1,16 +1,34 @@
 import { useGetMoviesQuery } from "@/redux/api/api";
 import { TMovie } from "@/types";
 import { MovieCard } from "../MovieCard/MovieCard";
+import { Skeleton } from "@/components/ui/skeleton"
+
 
 const TopMovies = () => {
   const { data: movies, isLoading } = useGetMoviesQuery({});
-  // console.log(movies);
+
+
   if (isLoading)
     return (
-      <p className="text-3xl text-center text-yellow-500 my-2 font-bold">
-        Loading....
-      </p>
+      <div className="my-5">
+        <h1 className="text-4xl font-bold text-yellow-400">What to watch</h1>
+        <h2 className="text-2xl font-bold my-2  border-l-4 border-l-yellow-400 px-1">
+          Top Rated Movies
+        </h2>
+        <div className="grid grid-cols-4 mt-8 justify-items-center">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <div key={index} className="flex flex-col space-y-3">
+              <Skeleton className="h-[200px] w-[250px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[250px]" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     );
+
   return (
     <div className="my-5">
       <h1 className="text-4xl font-bold text-yellow-400">What to watch</h1>
