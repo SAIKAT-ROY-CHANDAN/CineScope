ffef6609d9ced673cc8b0f0440a9b463490a7833
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-07-08 13:56:43 +0600

    feat:changes made in api.ts

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -7,64 +7,13 @@ export const baseApi = createApi({
   endpoints: (builder) => ({
     getMovies: builder.query({
       query: () => ({
-        url: "/movies",
         method: "GET",
-      }),
-    }),
-    getSingleMovie: builder.query({
-      query: (slug) => ({
-        url: `/movies/${slug}`,
-        method: "GET",
-      }),
-    }),
-    addMovies: builder.mutation({
-      query: (data) => ({
-        url: "/movies",
-        method: "POST",
-        body: data,
-      }),
-    }),
-    getMovieReviews: builder.query({
-      query: (slug) => ({
-        url: `/movies/${slug}/reviews`,
-        method: "GET",
-      }),
-    }),
-    getMovieDetailsAndReviews: builder.query({
-      queryFn: async (slug: string): Promise<any> => {
-        try {
-          const [movieResponse, reviewsResponse] = await Promise.all([
-            fetch(`http://localhost:5000/api/movies/${slug}`),
-            fetch(`http://localhost:5000/api/movies/${slug}/reviews`),
-          ]);
+        url: '/movies'
+      })
+    })
+  })
 
-          if (!movieResponse.ok || !reviewsResponse.ok) {
-            throw new Error("Network response was not ok.");
-          }
-          const [movieData, reviewsData] = await Promise.all([
-            movieResponse.json(),
-            reviewsResponse.json(),
-          ]);
+})
 
-          // Combine results
-          return {
-            data: {
-              movie: movieData,
-              reviews: reviewsData,
-            },
-          };
-        } catch (error) {
-          return error;
-        }
-      },
-    }),
-  }),
-});
 
-export const {
-  useGetMoviesQuery,
-  useGetSingleMovieQuery,
-  useAddMoviesMutation,
-  useGetMovieReviewsQuery,
-  useGetMovieDetailsAndReviewsQuery, 
-} = baseApi;
+export const {useGetMoviesQuery} = baseApi
